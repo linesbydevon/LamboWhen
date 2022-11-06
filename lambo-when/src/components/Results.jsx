0486263191564398267b65e26197244338d6541b
Devon Jones <linesbydevon@gmail.com>
2022-11-05 21:57:35 -0400

    more styling to results page

--- lambo-when/src/components/Results.jsx
+++ lambo-when/src/components/Results.jsx
@@ -35,7 +35,7 @@ export default function Results(props){
                 <div className="iconContainer">
                   <img src={coin.iconUrl} alt={`Logo for ${coin.name}`}/> 
                 </div>
-                <h3>{coin.name} | <span>{coin.symbol}</span></h3>
+                <h3>{coin.name==="Internet Computer (DFINITY)"? "ICP DFINITY ": coin.name} | <span>{coin.symbol}</span></h3>
               </div>
              
               
@@ -43,20 +43,19 @@ export default function Results(props){
 
               
               <div className="assetDetails">
-                <p className="value">${parseFloat(coin.price).toFixed(2)}</p>
-                <p className="perAsset">per coin</p>
-                <h4>24hr: <span className="indicator"></span>{(100-getPercentage(coin.sparkline[0],coin.sparkline[24])).toFixed(2)}%</h4>
+              <p className="change">24HR: <span className="indicator"></span>{(100-getPercentage(coin.sparkline[0],coin.sparkline[24])).toFixed(2)}%</p>
+                <h4 className="value">${parseFloat(coin.price).toFixed(2)}<br/><span>per coin</span></h4>
                 <div className="highLow">
+                <p className="low"><strong>Low:</strong><br/>${Math.min(...coin.sparkline).toFixed(2)}</p>
                   <p className="high"><strong>High:</strong><br/>${Math.max(...coin.sparkline).toFixed(2)}</p>
-                  <p className="low"><strong>Low:</strong><br/>${Math.min(...coin.sparkline).toFixed(2)}</p>
                 </div>
         <Sparklines className={coin.sparkline[24]>coin.sparkline[0]? "isUp":"isDown"} data={coin.sparkline}>
           <SparklinesLine color={coin.sparkline[24]>coin.sparkline[0]? "green":"red"} />
         </Sparklines>
         <div className={`portfolioSpecificInfo`}>
           <div className="qtyValue">
-            <p className="value">${coin.holdingsValue.toFixed(2)}</p>
-            <p className="perAsset">{coin.qty} {coin.symbol}</p>
+            <h4 className="value">${coin.holdingsValue.toFixed(2)}<br/><span>{coin.qty} {coin.symbol}</span></h4>
+            
             <div className="percentage">
               <div className="pieChart" style={{background: `conic-gradient(rgba(8, 10, 12,1) ${getPercentage(coin.holdingsValue, portfolio.value).toFixed(0)}%, rgba(8, 10, 12,.5) ${getPercentage(coin.holdingsValue, portfolio.value).toFixed(0)}%)`}}>
                 <div className="innerChart">
